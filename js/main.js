9318380fb58d13d99753fd4a38fe5c5d87513e10
Reynolds <reynolds@Reynoldss-MacBook-Pro.local>
2017-08-24 18:26:46 -0500

    second commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+document.getElementById("pageTitle").innerHTML = "Hello World"
+
+var myString = "My awesome project kinda sucks...";
+
+var myElement = document.getElementById("pageSubtitle");
+
+
+myElement.innerHTML = myString;
+
+// console loggin'
+
+var w = 42;
+var x;
+
+var y = 42;
+var z = "42";
+
+var answer = w + y;
+var answer2 = y + z;
+
+console.info("Now we're whatever");
+console.log(myElement);
+console.log(myString);
+console.log(x);
+console.log(y);
+console.log("-------------");
+console.log(answer);
